831998c4afa517bba778994fc0628d9da583d8c9
Mark M. Span <m.m.span@gmail.com>
2024-12-09 13:54:11 +0100

    newest python

--- Development/spectHR/__init__.py
+++ Development/spectHR/__init__.py
@@ -3,7 +3,7 @@ from spectHR.Plots.prepPlot import prepPlot
 
 from spectHR.Plots.Poincare import poincare
 from spectHR.Tools.Logger import logger, handler
-from spectHR.Tools.Webdav import copy_webdav
+from spectHR.Tools.Webdav import copyWebdav
 from spectHR.DataSet.SpectHRDataset import SpectHRDataset, TimeSeries
 from spectHR.Actions.csActions import *
 
